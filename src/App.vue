26d13a461f8e54fe1c5ec012455a6b6d94bb7562
Luis Angel <laj220704@gmail.com>
2025-06-06 17:05:35 -0600

    Logro: Primer commit

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 <div class="container">
         <h1>Reporte de Power BI</h1>
         <p>Visualización interactiva de
-            datos generada en Power BI.</p>
+            datos generada en Power BI desarrollada por Luis Angel Jimenez.</p>
         <div class="iframe-container">
 
            <iframe title="VisualizacionDataMartNorthwind" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTliOWMxNzItNWRkMy00NjBhLTk1MmUtNjkyOTZlYTBkNzY2IiwidCI6Ijg5NDk5NGZhLTg0MjEtNGZkNS05MmJiLTViYzA3NTdmNzlkZCJ9" frameborder="0" allowFullScreen="true"></iframe>
