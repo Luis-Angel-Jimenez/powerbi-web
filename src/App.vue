0582ed9e30f27cef0444f6f75f4863ea4ac71a50
Luis Angel <laj220704@gmail.com>
2025-06-06 16:40:14 -0600

    Logro: Primer commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="container">
+        <h1>Reporte de Power BI</h1>
+        <p>Visualización interactiva de
+            datos generada en Power BI.</p>
+        <div class="iframe-container">
+
+           <iframe title="VisualizacionDataMartNorthwind" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTliOWMxNzItNWRkMy00NjBhLTk1MmUtNjkyOTZlYTBkNzY2IiwidCI6Ijg5NDk5NGZhLTg0MjEtNGZkNS05MmJiLTViYzA3NTdmNzlkZCJ9" frameborder="0" allowFullScreen="true"></iframe>
+       
+        </div>
+    </div>
+</template>
